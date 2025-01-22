85e18e56a2f5c34f40629065adfea6e98fca9f77
piecekins <joshuagrushton@gmail.com>
2025-01-21 20:15:29 -0600

    place a

---
+++ cse110/adding_things.py
@@ -0,0 +1,15 @@
+#age=input('How old are you? ')
+#age_next_year=(int(age)+1)
+#print('On your next birthday, you will be '+str(age_next_year))
+#18print()
+#egg_cartons=input('How many egg cartons do you have? ')
+#eggs=(int(egg_cartons)*12)
+#print('You have '+str(eggs)+' eggs')
+#print()
+cookies=input('How many cookies do you have? ')
+people=input('How many people are there? ')
+cpp=(int(cookies)/int(people))
+print('Each person may have '+str(cpp)+' cookies')
+
+
+
